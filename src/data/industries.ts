6125d10e1cc602e4c4f0f3afb9d487410d131655
Jens Shumway <jensshum@gmail.com>
2025-01-03 14:32:58 -0700

    beans

--- src/data/industries.ts
+++ src/data/industries.ts
@@ -21,5 +21,31 @@ export const industries = [
   { id: 'elec', name: 'Electrical Services' },
   { id: 'land', name: 'Landscaping' },
   { id: 'photo', name: 'Photography Studios' },
-  { id: 'event', name: 'Event Planning' }
+  { id: 'event', name: 'Event Planning' },
+  
+  // Row 5
+  { id: 'med', name: 'Medical Clinics' },
+  { id: 'design', name: 'Interior Design' },
+  { id: 'it', name: 'IT Services' },
+  { id: 'tutor', name: 'Tutoring Services' },
+  
+  // Row 6
+  { id: 'hvac', name: 'HVAC Services' },
+  { id: 'account', name: 'Accounting Firms' },
+  { id: 'martial', name: 'Martial Arts Studios' },
+  { id: 'bakery', name: 'Bakeries' },
+  
+  // Row 7
+  { id: 'chiro', name: 'Chiropractors' },
+  { id: 'car', name: 'Car Dealerships' },
+  { id: 'insur', name: 'Insurance Agencies' },
+  { id: 'moving', name: 'Moving Services' },
+  
+  // Row 8
+  { id: 'spa', name: 'Day Spas' },
+  { id: 'arch', name: 'Architecture Firms' },
+  { id: 'pest', name: 'Pest Control' },
+  { id: 'market', name: 'Marketing Agencies' },
+
+  {id: 'other', name: 'Other'}
 ] as const;
