4b77f2b419ca1b17b2ac8bf32226e340893e3fbc
Jens Shumway <jensshum@gmail.com>
2025-01-03 13:45:55 -0700

    beans2

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import logoImage from "../../src/assets/LeadGenSuper (1).png";
 
 export const Logo = () => (
   <div className="flex items-center justify-center">
     <img 
-      src="../../src/assets/LeadGenSuper (1).png" 
+      src={logoImage} 
       alt="Lead Generator Super Logo"
-      className="h-16 w-auto"
+      className="h-12 w-auto"
     />
   </div>
 );
